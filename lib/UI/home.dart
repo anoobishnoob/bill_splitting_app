d7fa8b92a96ae640dd60700f604779a798bba113
Samme Qandil <sqandil@cornellpump.com>
2020-03-03 11:02:14 -0800

    added more stuff to UI

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -142,9 +142,23 @@ class _BillSplitterState extends State<BillSplitter> {
                                   ),
                                 ),
                               ),
-
-                            )
+                            ),
                           ],
+                        ),
+                      ],
+                    ),Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Tip",style: TextStyle(
+                            color: Colors.grey.shade700
+                        ),),
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Text(" \$34", style: TextStyle(
+                            color: _purple,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17
+                          ),),
                         )
                       ],
                     )
